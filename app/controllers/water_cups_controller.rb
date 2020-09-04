7bd32b92890df1e11bfae12c664d3cdf8b7ea210
candacecodes <candacehchoe@gmail.com>
2020-09-04 15:50:30 -0700

    updated watercup controller

--- app/controllers/water_cups_controller.rb
+++ app/controllers/water_cups_controller.rb
@@ -3,4 +3,24 @@ class WaterCupsController < ApplicationController
         watercup = WaterCup.all 
         render json: watercups
     end 
+
+    def show 
+        watercup = WaterCup.find_by_id(params[:id]) 
+        render json: watercup
+    end 
+
+    def new 
+        watercup = WaterCup.new
+    end 
+
+    def create 
+        watercup = WaterCup.new(watercup_params)
+        watercup.save 
+        render json: user 
+    end 
+
+end
+
+def watercup_params
+    params.require(:watercup).permit(:amount)
 end
